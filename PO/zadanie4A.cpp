c3838e54261e467d6a7dd9288d75e733ffa3566a
丂匚尺乇乇匚Ҝ <58401384+screeck@users.noreply.github.com>
2025-01-16 19:32:25 +0100

    Update zadanie4A.cpp

--- PO/zadanie4A.cpp
+++ PO/zadanie4A.cpp
@@ -1,117 +1,88 @@
 #include <iostream>
 #include <string>
-#include <vector>
 
-// Klasa abstrakcyjna ProduktPapierniczy
+
+using namespace std;
+
+
 class ProduktPapierniczy {
-public:
-    virtual void wypiszInformacje() const = 0; // Metoda czysto wirtualna
-    virtual ~ProduktPapierniczy() {} // Wirtualny destruktor
+
+
+
+
+
 };
 
-// Przykładowa klasa pochodna od ProduktPapierniczy
-class Dlugopis : public ProduktPapierniczy {
-    std::string kolorAtramentu;
 
-public:
-    Dlugopis(const std::string& kolor) : kolorAtramentu(kolor) {}
 
-    void wypiszInformacje() const override {
-        std::cout << "Długopis, kolor atramentu: " << kolorAtramentu << std::endl;
-    }
-};
 
 class SklepPapierniczy {
-    ProduktPapierniczy* produktyNaStanie[1600];
+
+private:
+    ProduktPapierniczy* produktNaStanie[1500];
     int aktualnaLiczbaProduktow;
 
 public:
-    SklepPapierniczy() : aktualnaLiczbaProduktow(0) {
-        for (int i = 0; i < 1600; ++i) {
-            produktyNaStanie[i] = nullptr;
-        }
-    }
 
-    bool dodajProdukt(ProduktPapierniczy* produkt) {
-        if (aktualnaLiczbaProduktow < 1600) {
-            produktyNaStanie[aktualnaLiczbaProduktow++] = produkt;
-            return true;
-        }
-        return false;
-    }
+    SklepPapierniczy() {};
 
-    void wypiszProdukty() const {
-        std::cout << "Produkty w sklepie:" << std::endl;
-        for (int i = 0; i < aktualnaLiczbaProduktow; ++i) {
-            if (produktyNaStanie[i]) {
-                produktyNaStanie[i]->wypiszInformacje();
-            }
-        }
-    }
 
-    ~SklepPapierniczy() {
-        for (int i = 0; i < aktualnaLiczbaProduktow; ++i) {
-            delete produktyNaStanie[i];
-        }
-    }
+    void dodajNaStan(ProduktPapierniczy* produkt) {};
+    void zdejmijZeStanu(ProduktPapierniczy* produkt) {};
+    ProduktPapierniczy* znajdzProdukt(string rodzaj, string nazwa) {};
+    void wyswietlProdukt() {};
+};
+
+
+
+class Pracownik {
+
+private:
+    string imie;
+    string nazwisko;
+    SklepPapierniczy* sklep;
+public:
+    Pracownik(string imie, string nazwisko, SklepPapierniczy* sklep) {};
+    string getimie();
+    string getNazwisko() {};
+    SklepPapierniczy* getSklep() {};
+    void setSklep(SklepPapierniczy* sklep) {};
+
+
+
+    friend class Sprzedawca;
+    friend class Zaopatrzeniowiec;
+
+};
+
+
+class Sprzedawca {
+public:
+    Sprzedawca(string imie, string nazwisko, SklepPapierniczy* sklep) {};
+    void sprzedajProdukt(string rodzaj, string nazwa) {};
+
 };
 
 class Zaopatrzeniowiec {
+private:
     ProduktPapierniczy* produktyNaPace[200];
     int aktualnaLiczbaProduktow;
-
 public:
-    Zaopatrzeniowiec() : aktualnaLiczbaProduktow(0) {
-        for (int i = 0; i < 200; ++i) {
-            produktyNaPace[i] = nullptr;
-        }
-    }
 
-    bool dodajNaPake(ProduktPapierniczy* produkt) {
-        if (aktualnaLiczbaProduktow < 200) {
-            produktyNaPace[aktualnaLiczbaProduktow++] = produkt;
-            return true;
-        }
-        return false;
-    }
+    Zaopatrzeniowiec(string imie, string nazwisko, SklepPapierniczy* sklep) {};
 
-    void wypiszPake() const {
-        std::cout << "Produkty na pace zaopatrzeniowca:" << std::endl;
-        for (int i = 0; i < aktualnaLiczbaProduktow; ++i) {
-            if (produktyNaPace[i]) {
-                produktyNaPace[i]->wypiszInformacje();
-            }
-        }
-    }
+    void zalodujProduktnaPake(ProduktPapierniczy* produkt) {};
+    void dostaczWszystkieProdukty() {};
 
-    ~Zaopatrzeniowiec() {
-        for (int i = 0; i < aktualnaLiczbaProduktow; ++i) {
-            delete produktyNaPace[i];
-        }
-    }
 };
 
+
+
+
+
+
 int main() {
-    SklepPapierniczy sklep;
-    Zaopatrzeniowiec zaopatrzeniowiec;
-
-    // Tworzenie produktów
-    ProduktPapierniczy* dlugopis1 = new Dlugopis("Niebieski");
-    ProduktPapierniczy* dlugopis2 = new Dlugopis("Czarny");
-
-    // Dodawanie produktów do zaopatrzeniowca
-    zaopatrzeniowiec.dodajNaPake(dlugopis1);
-    zaopatrzeniowiec.dodajNaPake(dlugopis2);
-
-    // Wypisanie zawartości paki zaopatrzeniowca
-    zaopatrzeniowiec.wypiszPake();
-
-    // Dodawanie produktów do sklepu
-    sklep.dodajProdukt(dlugopis1);
-    sklep.dodajProdukt(dlugopis2);
-
-    // Wypisanie zawartości sklepu
-    sklep.wypiszProdukty();
+  
 
     return 0;
 }
